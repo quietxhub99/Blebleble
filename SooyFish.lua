7730ff6e92fa8e5335abece66d7bda46dc9bc51b
quietxhub99 <hubquiet199>
2025-10-08 09:35:11 +0700

    update

--- SooyFish.lua
+++ SooyFish.lua
@@ -2257,7 +2257,7 @@ local FishCategories = {
 	["Mythic"] = {
 		"Gingerbread Shark","Loving Shark","King Crab","Blob Fish","Hermit Crab","Luminous Fish",
 		"Plasma Shark","Abyss Seahorse","Blueflame Ray","Hammerhead Shark","Hawks Turtle",
-		"Manta Ray","Loggerhead Turtle","Prismy Seahorse","Gingerbread Turtle", "Armor Catfish", "Armor Thin Shark", "Strippled Seahorse","Thresher Shark","Dotted Stingray", "Sharp One", "Hybodius Shark"
+		"Manta Ray","Loggerhead Turtle","Prismy Seahorse","Gingerbread Turtle", "Armor Catfish", "Armor Thin Shark", "Strippled Seahorse","Thresher Shark","Dotted Stingray", "Sharp One", "Hybodius Shark", "Magma Shark", "Crocodile", "Ancient Relic Crocodile", "Mammoth Appafish"
 	},
 	["Legendary"] = {
 		"Yellowfin Tuna","Lake Sturgeon","Lined Cardinal Fish","Saw Fish","Slurpfish Chromis","Chrome Tuna","Lobster", "Bumblebee Grouper","Lavafin Tuna","Blue Lobster","Greenbee Grouper","Starjam Tang","Magic Tang", "Enchanted Angelfish","Axolotl","Deep Sea Crab"
