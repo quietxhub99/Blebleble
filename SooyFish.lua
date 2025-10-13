77ecc842de5fe4da9cbdb795ba849b721512131a
quietxhub99 <hubquiet199>
2025-10-13 19:18:08 +0700

    update

--- SooyFish.lua
+++ SooyFish.lua
@@ -2100,6 +2100,32 @@ DStones:Button({
 ----- =======[ UTILITY TAB ]
 -------------------------------------------
 
+_G.RFRedeemCode = ReplicatedStorage.Packages._Index["sleitnick_net@0.2.0"].net["RF/RedeemCode"]
+
+_G.RedeemCodes = {
+    "BLAMETALON",
+    "FISHMAS2025",
+    "GOLDENSHARK",
+    "THANKYOU",
+}
+
+_G.RedeemAllCodes = function()
+    for _, code in ipairs(_G.RedeemCodes) do
+        local success, result = pcall(function()
+            return _G.RFRedeemCode:InvokeServer(code)
+        end)
+        task.wait(1)
+    end
+end
+
+Utils:Button({
+    Title = "Redeem All Codes",
+    Locked = false,
+    Callback = function()
+        _G.RedeemAllCodes()
+    end
+})
+
 local RFPurchaseMarketItem = ReplicatedStorage.Packages._Index["sleitnick_net@0.2.0"].net["RF/PurchaseMarketItem"]
 
 local merchantItems = {
@@ -2696,12 +2722,12 @@ FishNotif:Toggle({
 local FishCategories = {
 	["Secret"] = {
 		"Blob Shark","Great Christmas Whale","Frostborn Shark","Great Whale","Worm Fish","Robot Kraken",
-		"Giant Squid","Ghost Worm Fish","Ghost Shark","Queen Crab","Orca","Crystal Crab","Monster Shark","Eerie Shark", "Scare", "Thin Armor Shark", "Orca", "Lochness Monster", "Megalodon", "Phanter Eel", "Mosasaur Shark", "King Jelly", "Bone Whale", "Elshark Gran Maja"
+		"Giant Squid","Ghost Worm Fish","Ghost Shark","Queen Crab","Orca","Crystal Crab","Monster Shark","Eerie Shark", "Scare", "Thin Armor Shark", "Orca", "Lochness Monster", "Megalodon", "Phanter Eel", "Mosasaur Shark", "King Jelly", "Bone Whale", "Elshark Gran Maja", "Ancient Whale"
 	},
 	["Mythic"] = {
 		"Gingerbread Shark","Loving Shark","King Crab","Blob Fish","Hermit Crab","Luminous Fish",
 		"Plasma Shark","Abyss Seahorse","Blueflame Ray","Hammerhead Shark","Hawks Turtle",
-		"Manta Ray","Loggerhead Turtle","Prismy Seahorse","Gingerbread Turtle", "Armor Catfish", "Armor Thin Shark", "Strippled Seahorse","Thresher Shark","Dotted Stingray", "Sharp One", "Hybodius Shark", "Crocodile", "Ancient Relic Crocodile", "Mammoth Appafish"
+		"Manta Ray","Loggerhead Turtle","Prismy Seahorse","Gingerbread Turtle", "Armor Catfish", "Armor Thin Shark", "Strippled Seahorse","Thresher Shark","Dotted Stingray", "Sharp One", "Hybodius Shark", "Crocodile", "Ancient Relic Crocodile", "Mammoth Appafish", "Crystal Salamender"
 	},
 	["Legendary"] = {
 		"Yellowfin Tuna","Lake Sturgeon","Lined Cardinal Fish","Saw Fish","Slurpfish Chromis","Chrome Tuna","Lobster", "Bumblebee Grouper","Lavafin Tuna","Blue Lobster","Greenbee Grouper","Starjam Tang","Magic Tang", "Enchanted Angelfish","Axolotl","Deep Sea Crab", "Temple Spokes Tuna", "Manoai Statue Fish"
